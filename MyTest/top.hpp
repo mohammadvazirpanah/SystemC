85476c6691f1d7b94b4f3d798b7325ed12c2b169
mohammad <mohammad.vazirpanah@yahoo.com>
2022-04-19 11:53:09 +0430

    Try to debug-Define module after simulation start

--- MyTest/top.hpp
+++ MyTest/top.hpp
@@ -1,11 +1,33 @@
 #include <systemc.h>
 
+SC_MODULE(Mytest)
+{
+
+void prnt()
+    {
+        std::cout<<"My Second Module\n";
+
+    }
+
+    SC_CTOR(Mytest)
+    {
+        //SC_THREAD(prnt);
+    }
+};
+
 SC_MODULE(top)
 {
 
+void test()
+    {
+        std::cout<<"My First Module\n";
+        wait(10,SC_NS);
+        Mytest test1 ("test1");
 
-SC_CTOR(top)
-{
-    
-}
+    }
+
+    SC_CTOR(top)
+    {
+        SC_THREAD(test);
+    }
 };
