183e544734c5ddb892c3fd7cf91cde2303dcf1f9
mohammad <mohammad.vazirpanah@yahoo.com>
2022-04-26 01:54:02 +0430

    Writing with Macro

--- spawn_process/top.hpp
+++ spawn_process/top.hpp
@@ -14,23 +14,43 @@ using namespace std;
 
 SC_MODULE(top) 
 {
-	
+
+
+    void publisher ()
+{
+    int argc; 
+    char **argv;
+    ros::init(argc, argv, "publish");
+    ros::start();
+    ros::NodeHandle n;
+    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+    geometry_msgs::Twist msg;
+    std::cout <<"Test";
+    msg.linear.x = 5;
+    msg.linear.y = 0;
+    msg.linear.z = 0;
+    pub.publish(msg);
+    ros::spin();
+}
+
+
 	SC_CTOR(top) 
 
 	{	
-        int argc; 
-        char **argv;
-        ros::init(argc, argv, "publish");
-        ros::start();
-        ros::NodeHandle n;
-        ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
-        geometry_msgs::Twist msg;
-        msg.linear.x = 5;
-        msg.linear.y = 0;
-        msg.linear.z = 0;
-        pub.publish(msg);
+        // int argc; 
+        // char **argv;
+        // ros::init(argc, argv, "publish");
+        // ros::start();
+        // ros::NodeHandle n;
+        // ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+        // geometry_msgs::Twist msg;
+        // msg.linear.x = 5;
+        // msg.linear.y = 0;
+        // msg.linear.z = 0;
+        // pub.publish(msg);
         
-        ros::spin();
+        // ros::spin();
+        SC_THREAD (publisher);
 	}
 
 };
@@ -48,40 +68,3 @@ SC_MODULE(top)
 //     }
 
 
-void publisher ()
-{
-    int argc; 
-    char **argv;
-    ros::init(argc, argv, "publish");
-    ros::start();
-    ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
-    // ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    // ros::Rate loop_rate(10);
-    // std_msgs::String msg;
-    // std::stringstream ss;
-    geometry_msgs::Twist msg;
-    std::cout <<"Test";
-    msg.linear.x = 5;
-    msg.linear.y = 0;
-    msg.linear.z = 0;
-
-    // msg.angular.z = 5;
-    for (int i =0; i<3 ; i++)
-    pub.publish(msg);
-    ros::spin();
-
-    // for (int i=0; i++ ; i<10)
-    // {
-        // msg.data = ss.str();
-        // ss << "hello world " << i;
-        // ROS_INFO("%s", msg.data.c_str());
-    // std_msgs::Float64 msg1;
-    // msg1.data = 10.0;
-    // chatter_pub.publish(msg1);
-        // wait(10, SC_SEC);
-        // loop_rate.sleep();
-    // }
-    // chatter_pub.publish(msg);
-    // loop_rate.sleep();
-}
